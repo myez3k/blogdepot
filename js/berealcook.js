d624e2f36a537d1a2387d1ebfb9064015b83b3ba
myez3k <127506112+myez3k@users.noreply.github.com>
2023-05-05 12:33:57 -0400

    Add files via upload

---
+++ js/berealcook.js
@@ -0,0 +1,7 @@
+/*<![CDATA[*/ var K2C_box=document.querySelector("#K2C_box"),Cookie_btn=document.querySelector("#Cookie_btn"),ckErrMes="Cookie can't be set! Please unblock this site from the cookie setting of your browser.";if(null!=K2C_box){Cookie_btn.onclick=()=>{document.cookie="CookieConsentByFineshop=Accepted; max-age=2592000; path=/",document.cookie?K2C_box.classList.add("acptd"):alert(ckErrMes)};let e=document.cookie.indexOf("CookieConsentByFineshop=Accepted");-1!=e?K2C_box.classList.add("k2hide"):K2C_box.classList.remove("k2hide")} /*]]>*/
+
+
+
+
+
+
